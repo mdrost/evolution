688f17ce12b91859b33eecf232fca6cdde9553fb
Mateusz Drost <mat.drost@gmail.com>
2016-04-07 06:36:00 +0200

    Grid.h: small refactor

--- Grid.h
+++ Grid.h
@@ -84,24 +84,15 @@ public:
 	MooreNeighborhood<value_type, 2*Radius+1> mooreNeighborhoodAt(int rowIndex, int colIndex)
 	{
 		assert(rowIndex < mRowCount && colIndex < mColumnCount);
+		if (__builtin_expect(needWraparound<Radius>(rowIndex, colIndex), 0)) {
+			return mooreNeighborhoodWraparoundedAt<Radius>(rowIndex, colIndex);
+		}
 		MooreNeighborhood<value_type, 2*Radius+1> result;
-		if (__builtin_expect(needWraparound(rowIndex, colIndex, Radius), 0)) {
-			for (int r = 0 ; r < (2*Radius+1); ++r) {
-				int row = rowIndex - (Radius - r);
-				row = wraparoundRow(row);
-				for (int c = 0 ; c < (2*Radius+1); ++c) {
-					int col = colIndex - (Radius - c);
-					col = wraparoundColumn(col);
-					result[r][c] = at(row, col);
-				}
-			}
-		} else {
-			for (int r = 0 ; r < (2*Radius+1); ++r) {
-				int row = rowIndex - (Radius - r);
-				for (int c = 0 ; c < (2*Radius+1); ++c) {
-					int col = colIndex - (Radius - c);
-					result[r][c] = at(row, col);
-				}
+		for (int r = 0 ; r < (2*Radius+1); ++r) {
+			int row = rowIndex - (Radius - r);
+			for (int c = 0 ; c < (2*Radius+1); ++c) {
+				int col = colIndex - (Radius - c);
+				result[r][c] = at(row, col);
 			}
 		}
 		return result;
@@ -120,12 +111,35 @@ public:
 
 private:
 
-	bool needWraparound(int rowIndex, int colIndex, int radius) const noexcept
+	template <size_type Radius>
+	MooreNeighborhood<value_type, 2*Radius+1> mooreNeighborhoodWraparoundedAt(int rowIndex, int colIndex)
+	{
+		assert(rowIndex < mRowCount && colIndex < mColumnCount);
+		MooreNeighborhood<value_type, 2*Radius+1> result;
+		for (int r = 0 ; r < (2*Radius+1); ++r) {
+			int row = rowIndex - (Radius - r);
+			row = wraparoundRow(row);
+			for (int c = 0 ; c < (2*Radius+1); ++c) {
+				int col = colIndex - (Radius - c);
+				col = wraparoundColumn(col);
+				result[r][c] = at(row, col);
+			}
+		}
+		return result;
+	}
+
+	constexpr bool needWraparound(int rowIndex, int colIndex, int radius) const noexcept
 	{
 		return (rowIndex < radius)
 		|| (colIndex < radius)
 		|| (rowIndex >= (mRowCount - radius))
 		|| (colIndex >= (mColumnCount - radius));
+	}
+
+	template <int Radius>
+	constexpr bool needWraparound(int rowIndex, int colIndex) const noexcept
+	{
+		return needWraparound(rowIndex, colIndex, Radius);
 	}
 
 	int wraparound(int index, int dimensionSize) const noexcept
